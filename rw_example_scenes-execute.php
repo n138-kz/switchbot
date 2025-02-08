42f579b1f43175360b07f6f20046b2df3d285a92
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-02-08 21:30:22 +0900

    clearstatcache

--- rw_example_scenes-execute.php
+++ rw_example_scenes-execute.php
@@ -52,6 +52,7 @@ $sign = strtoupper(base64_encode($sign));
 $activates=__DIR__.'/.secret/activate_scenes.json';
 try {
     $activates=__DIR__.'/.secret/activate_scenes.json';
+    clearstatcache(TRUE);
     if(!file_exists($activates)){ throw new \Exception('Config file does not exist.'); }
     if(!is_readable($activates)){ throw new \Exception('Config file does readable.'); }
     if(!filesize($activates)){ throw new \Exception('Config file does readable.'); }
