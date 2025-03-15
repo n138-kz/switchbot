5dd29f04f274d76e42701eebae8e77fdb7f0a79f
Komiya Yu <komiya.yu@n138.jp>
2025-03-16 07:25:28 +0900

    message

--- server/index.php
+++ server/index.php
@@ -223,6 +223,11 @@ if (false) {
 	$webapp_client->result['data'] = [
 		$webapp_client->result['data_id']=>$switchbot->getDevices($token,$sign,$nonce,$t,$sceneId=null),
 	];
+	if(is_null($webapp_client->result['data'])){
+		$webapp_client->result['message'] = 'API Credential has invalid.';
+	} else {
+		$webapp_client->result['message'] = 'Getted the API data.';
+	}
 	echo json_encode($webapp_client->result_return(), JSON_OPTION_ENCODE);
 } elseif ($accessmode=='scenes_list') {
 	$webapp_client->result['data_id'] = 'switchbot.'.$accessmode;
