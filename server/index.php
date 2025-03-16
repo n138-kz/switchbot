4896d31c1a9bf6257696e8b0610336fc05dc579c
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-03-16 10:15:25 +0900

    is_empty sceneId
    https://qiita.com/shinichi-takii/items/00aed26f96cf6bb3fe62

--- server/index.php
+++ server/index.php
@@ -261,7 +261,7 @@ if (false) {
 	echo json_encode($webapp_client->result_return(), JSON_OPTION_ENCODE);
 } elseif (strtolower( $_SERVER['REQUEST_METHOD'] ) == 'post' && $accessmode=='scene_activate') {
 	$sceneId = isset($_POST['scene_id'])?$_POST['scene_id']:''
-	if(is_empty($sceneId)){
+	if($sceneId){
 		$webapp_client->result['message'] = 'API scene_id has empty. this params is required.';
 		echo json_encode($webapp_client->result_return(), JSON_OPTION_ENCODE);
 		exit(1);
