67df7999cb122ed11bfd2202fb7659a3abdc500c
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-03-16 10:12:33 +0900

    curl

--- server/index.php
+++ server/index.php
@@ -140,6 +140,14 @@ class switchbot{
 		);
 
 		$url = "https://api.switch-bot.com/v1.1/scenes/${sceneId}/execute";
+
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		$response = curl_exec($curl);
+		curl_close($curl);
 	}
 }
 date_default_timezone_set('Asia/Tokyo');
