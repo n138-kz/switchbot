aa94d3e9aa7755af392442346eca2f3f1229e5d9
Komiya Yu <komiya.yu@n138.jp>
2025-03-30 11:19:08 +0900

    columns add: evented_on, scene_id, result_status

--- server/create-table_switchbot.sql
+++ server/create-table_switchbot.sql
@@ -3,5 +3,8 @@ CREATE TABLE IF NOT EXISTS switchbot
 	"timestamp" double precision NOT NULL UNIQUE PRIMARY KEY DEFAULT EXTRACT(epoch FROM CURRENT_TIMESTAMP),
 	remote_addr text NOT NULL,
 	remote_port text NOT NULL,
-	useragent text NOT NULL
+	useragent text NOT NULL,
+	evented_on text NOT NULL,
+	scene_id text,
+	result_status boolean
 );
