293ef4016782ffc9d8557e5b6798a766f0770482
Komiya Yu <komiya.yu@n138.jp>
2025-05-03 20:22:40 +0900

    runcount=10 default

--- server/index.php
+++ server/index.php
@@ -428,7 +428,7 @@ $webapp_client->result['file']=null;
 $webapp_client->result['discord_webhook_event']=null;
 
 $accessmode=isset($_GET['accessmode'])?$_GET['accessmode']:'scenes_list';
-$runcount=isset($_GET['runcount'])?$_GET['runcount']:1;
+$runcount=isset($_GET['runcount'])?$_GET['runcount']:10;
 $displayformat=isset($_GET['displayformat'])?$_GET['displayformat']:'json';
 if (false) {
 } elseif ($accessmode==''&&false) {
